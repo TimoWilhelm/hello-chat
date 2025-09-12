9f8ee6883f1009a960d11df7a86d9346c2b376b9
Timo Wilhelm <twilhelm@cloudflare.com>
2025-09-12 14:59:11 +0200

    actor ws

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Actor, ActorConfiguration, handler } from '@cloudflare/actors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+export class Chat extends Actor<Env> {
+	static configuration: (request: Request) => ActorConfiguration = (request) => {
+		return {
+			sockets: {
+				upgradePath: '/ws',
+				autoResponse: {
+					ping: 'pong',
+					pong: 'ping',
+				},
+			},
+		};
+	};
+
+	protected shouldUpgradeSocket(request: Request): boolean {
+		return true;
+	}
+
+	protected onSocketConnect(ws: WebSocket, request: Request) {
+		const url = new URL(request.url);
+		console.log('Socket connected', url.searchParams.get('name'));
+	}
+
+	protected onSocketDisconnect(ws: WebSocket) {
+		console.log('Socket disconnected');
+	}
+
+	protected onSocketMessage(ws: WebSocket, data: any) {
+		const parsed = JSON.parse(data);
+		const name = ws.deserializeAttachment().queryParams.name;
+		this.sockets.message(JSON.stringify({ name, ...parsed }), '*', [ws]);
+	}
+}
+
+export default handler(Chat);
